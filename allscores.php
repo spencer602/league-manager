ee07defc6b5c0c00d941e379329faa72b1484d00
Spencer DeBuf <spencer602@gmail.com>
2020-01-10 16:51:56 -0700

    added functions to calculate the points from the matches, calculate win percentage, sort based on win percentage, and display win percentage against players about a specified win percentage

---
+++ allscores.php
@@ -0,0 +1,60 @@
+<?php
+include 'calculatescore.php';
+
+function allDataAllPlayers() {
+    $dbhost  = 'localhost';
+
+    $dbname  = 'sharkhunt';   // Modify these...
+    $dbuser  = 'spencer';   // ...variables according
+    $dbpass  = 'salute';   // ...to your installation
+
+    $connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+    if ($connection->connect_error)
+        die("Fatal Error 1");
+    $allStandings = mysqli_query($connection, "SELECT * from players ORDER BY points");
+
+    $allPlayers = array();
+
+// position is the players position in the rankings
+    $position = 0;
+    echo '<table id="standingsTable">';
+    while ($row = $allStandings->fetch_assoc()) {
+        $name = $row['player_name'];
+        $playerData =getDataForPlayerID($row['player_id']);
+        $oldPoints = $row['points'];
+        $points = 250 + $playerData[0];
+
+        $playerRow = array($name, $row['player_id'], $points, $playerData[1], $playerData[2], $playerData[3], $playerData[4], $playerData[5]);
+        array_push($allPlayers, $playerRow);
+
+        $wp = $playerData[1]/$playerData[2];
+
+        echo "$name: Points: $points, Matches: ($wp) $playerData[1]/$playerData[2], Games: $playerData[3]/$playerData[4], ERO: $playerData[5] <br>";
+
+    }
+    return $allPlayers;
+}
+
+$alldata = allDataAllPlayers();
+
+usort($alldata, "sortByWinPercentage");
+
+for ($i = 0; $i < count($alldata); $i++) {
+    $thisRow = $alldata[$i];
+    $name = $thisRow[0];
+    $wp = $thisRow[5]/$thisRow[6];
+    $gamesPlayed = $thisRow[6];
+
+    $wpAgainstBetter = getWinPercentageGreaterThan(.5, $thisRow[1]);
+    if ($thisRow[6] == 0) $wp = "NA";
+    echo "$name: $wp ($gamesPlayed)    Win Percentage against > .500 : $wpAgainstBetter<br>";
+}
+
+function sortByWinPercentage($a, $b) {
+    $awp = (float)$a[5]/(float)$a[6];
+    $bwp = (float)$b[5]/(float)$b[6];
+
+    if ($awp == $bwp) return 0;
+    return ($awp < $bwp) ? -1 : 1;
+
+}
---
+++ calculatescore.php
@@ -0,0 +1,147 @@
+<?php
+
+function getDataForPlayerID($playerID) {
+    $dbhost  = 'localhost';
+
+    $dbname  = 'sharkhunt';   // Modify these...
+    $dbuser  = 'spencer';   // ...variables according
+    $dbpass  = 'salute';   // ...to your installation
+
+    $connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+    if ($connection->connect_error)
+        die("Fatal Error 1");
+
+//$playerID = "5";
+
+// all fields of all matching queries
+    $query = "SELECT p1.player_name AS p1name, p2.player_name AS p2name, p1_points_wagered, p2_points_wagered, 
+    p1_games_needed, p2_games_needed, p1_games_won, p2_games_won, p1_ero, p2_ero, date_and_time, location_played, p1_id, p2_id 
+            FROM players p1, matches, players p2 
+            WHERE p1.player_id = p1_id AND p2.player_id = p2_id AND (p1_id = '$playerID' OR p2_id = '$playerID')
+            ORDER BY date_and_time DESC;";
+    $allMatches = mysqli_query($connection, $query);
+
+//    $resultArray = array();
+//    $tempArray = array();
+    $p2Array = array();
+
+    $playerPoints = 0;
+//    $opponentPoints = 0;
+    $playerMatchWins = 0;
+    $playerMatches = 0;
+    $playerGameWins = 0;
+    $playerGames = 0;
+    $playerEROs = 0;
+
+
+    while ($row = $allMatches->fetch_assoc()) {
+
+        $p1points = 0;
+        $p2points = 0;
+
+        $p1MatchWins = 0;
+        $p2MatchWins = 0;
+
+        $playerMatches += 1;
+        $playerGames += $row['p1_games_won'] + $row['p2_games_won'];
+
+        $p1IsWinner = false;
+        // determines the winner
+        if ($row['p1_games_won'] == $row['p1_games_needed']) {
+            $p1IsWinner = true;
+        }
+
+        // update the stats based on winner: assigning points (even if there is a point handicap given)
+        // NOTE: if p1 is winner, then the number of points won/lost by each is determined by how much player2 wagered
+        if ($p1IsWinner) {
+            $p1points += $row['p2_points_wagered'];
+            $p2points -= $row['p2_points_wagered'];
+            $p1MatchWins += 1;
+        } else {    // NOTE: if p2 is winner, then the number of points won/lost by each is determined by how much player1 wagered
+            $p1points -= $row['p1_points_wagered'];
+            $p2points += $row['p1_points_wagered'];
+            $p2MatchWins += 1;
+        }
+
+        if ($row['p1_id'] == $playerID) {
+            $playerPoints += $p1points;
+            $playerMatchWins += $p1MatchWins;
+            $playerGameWins += $row['p1_games_won'];
+            $playerEROs += $row['p1_ero'];
+        }
+        else if ($row['p2_id'] == $playerID) {
+            $playerPoints += $p2points;
+            $playerMatchWins += $p2MatchWins;
+            $playerGameWins += $row['p2_games_won'];
+            $playerEROs += $row['p2_ero'];
+        }
+
+//    $name = $row['p2name'];
+//
+//    array_push($p2Array, $name);
+
+        // Add each row into our results array
+//    $tempArray = $row;
+//    array_push($resultArray, $tempArray);
+    }
+//    $count = count($p2Array);
+//    echo "21 points: $playerPoints";
+
+//    return $playerPoints;
+
+    $playerData = array($playerPoints, $playerMatchWins, $playerMatches, $playerGameWins, $playerGames, $playerEROs);
+
+    return $playerData;
+}
+
+function getWinPercentageGreaterThan($winPercentageThreshold, $playerID) {
+    $dbhost  = 'localhost';
+
+    $dbname  = 'sharkhunt';   // Modify these...
+    $dbuser  = 'spencer';   // ...variables according
+    $dbpass  = 'salute';   // ...to your installation
+
+    $connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+    if ($connection->connect_error)
+        die("Fatal Error 1");
+
+//$playerID = "5";
+
+// all fields of all matching queries
+    $query = "SELECT p1.player_name AS p1name, p2.player_name AS p2name, p1_points_wagered, p2_points_wagered, 
+    p1_games_needed, p2_games_needed, p1_games_won, p2_games_won, p1_ero, p2_ero, date_and_time, location_played, p1_id, p2_id 
+            FROM players p1, matches, players p2 
+            WHERE p1.player_id = p1_id AND p2.player_id = p2_id AND (p1_id = '$playerID' OR p2_id = '$playerID')
+            ORDER BY date_and_time DESC;";
+    $allMatches = mysqli_query($connection, $query);
+
+//    $resultArray = array();
+//    $tempArray = array();
+    $p2Array = array();
+
+    $playerPoints = 0;
+//    $opponentPoints = 0;
+    $playerMatchWins = 0;
+    $playerMatches = 0;
+    $playerGameWins = 0;
+    $playerGames = 0;
+    $playerEROs = 0;
+
+
+    while ($row = $allMatches->fetch_assoc()) {
+        $opponentID = $row['p1_id'];
+        if ($playerID == $opponentID) $opponentID = $row['p2_id'];
+        $opponentData = getDataForPlayerID($opponentID);
+        $opponentWinPercentage = $opponentData[3] / $opponentData[4];
+        if ($opponentWinPercentage > $winPercentageThreshold) {
+            $playerGames += $row['p1_games_won'] + $row['p2_games_won'];
+            if ($playerID == $row['p1_id']) $playerGameWins += $row['p1_games_won'];
+            if ($playerID == $row['p2_id']) $playerGameWins += $row['p2_games_won'];
+        }
+    }
+
+    if ($playerGames == 0) return "NA";
+    return $playerGameWins / $playerGames;
+}
+
+

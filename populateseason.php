82c32ab40734e0d69c8c05af8c6ec0d75dd09ee1
Spencer DeBuf <spencer602@gmail.com>
2020-01-16 20:29:28 -0700

    added file with script for adding each player to season 1

---
+++ populateseason.php
@@ -0,0 +1,27 @@
+<?php
+$dbhost  = 'localhost';
+
+$dbname  = 'sharkhunt';   // Modify these...
+$dbuser  = 'spencer';   // ...variables according
+$dbpass  = 'salute';   // ...to your installation
+
+$connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+if ($connection->connect_error)
+    die("Fatal Error 1");
+
+$allPlayers = mysqli_query($connection, "SELECT player_id from players");
+$position = 0;
+
+while ($row = $allPlayers->fetch_assoc()) {
+    $id = $row['player_id'];
+
+    mysqli_query($connection, "INSERT INTO registrations
+(season_id, player_id)
+  VALUES
+  (1, $id);");
+
+    mysqli_query($connection, "UPDATE registrations SET paid = 1");
+
+//    mysqli_query($connection, "UPDATE players SET points = 250, games_played = 0, games_won = 0, eros = 0,
+//        matches_played = 0, matches_won = 0 WHERE player_id = $id");
+}

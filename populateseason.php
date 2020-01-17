47a2ee0a9cdf75ad4e0956997b5eb8a182e7ac09
Spencer DeBuf <spencer602@gmail.com>
2020-01-16 21:16:22 -0700

    fixed script to add all players to season 1

--- populateseason.php
+++ populateseason.php
@@ -16,11 +16,11 @@ while ($row = $allPlayers->fetch_assoc()) {
     $id = $row['player_id'];
 
     mysqli_query($connection, "INSERT INTO registrations
-(season_id, player_id)
+(season_id, player_id, paid)
   VALUES
-  (1, $id);");
+  (1, $id, 1);");
 
-    mysqli_query($connection, "UPDATE registrations SET paid = 1");
+    //mysqli_query($connection, "UPDATE registrations SET paid = 1");
 
 //    mysqli_query($connection, "UPDATE players SET points = 250, games_played = 0, games_won = 0, eros = 0,
 //        matches_played = 0, matches_won = 0 WHERE player_id = $id");
